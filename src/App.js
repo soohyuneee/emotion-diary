fe881ae2b9113a84685471f6d3599302ba97268f
soohyuneee <tlstngus95@naver.com>
2023-02-14 00:17:27 +0900

    타이틀 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useReducer, useRef, useEffect} from 'react';
-import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
